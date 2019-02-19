a5522265adea2c0f602404bd87bdf19cece8f540
bootcampglo <gloria.martinez.guijarro@gmail.com>
2019-02-19 17:55:21 +0100

    validando publicar trrayecto

--- src/app/publicar-trayecto/publicar-trayecto.component.ts
+++ src/app/publicar-trayecto/publicar-trayecto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'publicar-trayecto',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publicar-trayecto.component.css']
 })
 export class PublicarTrayectoComponent implements OnInit {
-
+  formTrayecto: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.formTrayecto = new FormGroup({
+      origenTrayecto: new FormControl('',[
+       Validators.required
+      ]),
+      destinoTrayecto: new FormControl('',[
+        Validators.required,
+      ]),
+      fechaTrayecto: new FormControl('',[
+        Validators.required, 
+        this.fechaEval
+      ]),
+      horaTrayecto: new FormControl('',[
+        Validators.required,
+      ]),
+    })
   }
 
+  fechaEval(control){
+    if(control.value < 2019){
+     return {'fechaTrayecto': 'no puedes viajar al pasado'}
+    }
+    return null
+  }
+
+  manejarFomuTrayecto(){
+    console.log(this.formTrayecto.value)
+  }
 }
